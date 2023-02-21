150a28d9806dcb79bc917c326c0454a24cdd7377
Peter Huynh <peterhyh@yahoo.com>
2023-02-21 14:50:00 -0600

    Clicker and reset button update

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,12 +1,41 @@
-import { View } from 'react-native';
-import HomeScreen from './HomeScreen';
+import { View, StyleSheet, Image, Text, Button, TouchableHighlight } from 'react-native';
+import button from '../assets/catball.png';
+import { useState } from 'react';
+import background from '../assets/floor.png';
 
-const MainScreen = () => {
+const Main = () => {
+    const [counter, setCounter] = useState(0);
+
     return (
-        <View>
-            <HomeScreen />
+        <View style={styles.view}>
+            <Text>Clicks: {counter}</Text>
+            <TouchableHighlight
+                underlayColor={0}
+                style={styles.highlight}
+                onPress={() => setCounter(counter + 1)}
+            >
+                <Image style={styles.button} source={button} />
+            </TouchableHighlight>
+            <Button style={styles.reset} title='Reset' onPress={() => setCounter(0)} />
         </View>
     )
 };
 
-export default MainScreen;
+const styles = StyleSheet.create({
+    view: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    button: {
+        width: 500,
+        height: 500,
+        alignItems: 'center'
+    },
+    reset: {
+        backgroundColor: '#000',
+        alignItems: 'center'
+    }
+})
+
+export default Main;
