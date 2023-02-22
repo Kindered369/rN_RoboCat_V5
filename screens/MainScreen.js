4bbad7ad93be04f5d842c49a850040fa33712e15
Peter Huynh <peterhyh@yahoo.com>
2023-02-22 10:56:27 -0600

    Changing avatar update

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,22 +1,37 @@
-import { View, StyleSheet, Image, Text, Button, TouchableHighlight } from 'react-native';
+import { View, StyleSheet, Image, Text, Button, TouchableHighlight, ImageBackground } from 'react-native';
 import button from '../assets/catball.png';
-import { useState } from 'react';
+import button2 from '../assets/catballD.png';
+import button3 from '../assets/catballDW.png';
+import { useState, useEffect } from 'react';
 import background from '../assets/floor.png';
 
 const Main = () => {
     const [counter, setCounter] = useState(0);
+    const [clicker, setClicker] = useState(button);
+
+    useEffect(() => {
+        if (counter >= 10 && counter < 20) {
+            setClicker(button2)
+        } else if (counter >= 20) {
+            setClicker(button3)
+        } else {
+            setClicker(button)
+        }
+    }, [counter])
 
     return (
         <View style={styles.view}>
-            <Text>Clicks: {counter}</Text>
-            <TouchableHighlight
-                underlayColor={0}
-                style={styles.highlight}
-                onPress={() => setCounter(counter + 1)}
-            >
-                <Image style={styles.button} source={button} />
-            </TouchableHighlight>
-            <Button style={styles.reset} title='Reset' onPress={() => setCounter(0)} />
+            <ImageBackground style={styles.view} source={background} resizeMode='cover'>
+                <Text>Clicks: {counter}</Text>
+                <TouchableHighlight
+                    underlayColor={0}
+                    style={styles.highlight}
+                    onPress={() => setCounter(counter + 1)}
+                >
+                    <Image style={styles.button} source={clicker} />
+                </TouchableHighlight>
+                <Button style={styles.reset} title='Reset' onPress={() => setCounter(0)} />
+            </ImageBackground>
         </View>
     )
 };
@@ -34,7 +49,8 @@ const styles = StyleSheet.create({
     },
     reset: {
         backgroundColor: '#000',
-        alignItems: 'center'
+        alignItems: 'center',
+        color: 'green'
     }
 })
 
